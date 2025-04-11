eabcd12eebce2c6bfaac9914b59d5439dd5ece5e
ashworks1706 <asriv132@asu.edu>
2025-04-10 23:16:17 -0700

    minstack

---
+++ NeatCode/stack/minstack.py
@@ -0,0 +1,25 @@
+class MinStack:
+    def __init__(self):
+        self.stack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+minStack = MinStack()
+minStack.push(1)
+minStack.push(2)
+minStack.push(0)
+print(minStack.getMin())
+
+minStack.pop()
+print(minStack.top())  
+print(minStack.getMin())
